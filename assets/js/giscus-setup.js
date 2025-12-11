e2c6ae8e44e1ba896fe7360eb390212177bd69b9
jeonso0907 <jeonso3412@gmail.com>
2025-12-11 04:09:35 +0000

    Deploying to gh-pages from @ jeonso0907/jeonso0907.github.io@886f2875d0ca7646d43708ec383668e36f0a69a1 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "jeonso0907/sooyoung-jeon.github.io",
+    "data-repo": "jeonso0907/jeonso0907.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
